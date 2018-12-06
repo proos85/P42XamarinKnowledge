58c8e33a747fea4fe0ca5d3b9487ee50e321ce91
Patrick Roos <patrick@point42.nl>
2018-12-06 09:08:28 +0100

    DelegateCommand

--- P42XamarinPrism/P42XamarinPrism/ViewModels/MainPageViewModel.cs
+++ P42XamarinPrism/P42XamarinPrism/ViewModels/MainPageViewModel.cs
@@ -1,14 +1,56 @@
-﻿using Prism.Navigation;
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Prism.Navigation;
 using P42XamarinPrism.Helpers;
+using Prism.Commands;
+using Prism.Services;
+using Xamarin.Forms;
 
 namespace P42XamarinPrism.ViewModels
 {
     public class MainPageViewModel : ViewModelBase
     {
-        public MainPageViewModel(INavigationService navigationService)
+        private readonly IPageDialogService _pageDialogService;
+        private bool _isAwesomeButtonEnabled = false;
+        public bool IsAwesomeButtonEnabled
+        {
+            get => _isAwesomeButtonEnabled;
+            set => SetProperty(ref _isAwesomeButtonEnabled, value);
+        }
+
+        public ICommand AwesomeButtonCommand { get; set; }
+
+        public MainPageViewModel(
+            INavigationService navigationService,
+            IPageDialogService pageDialogService)
             : base(navigationService)
         {
+            _pageDialogService = pageDialogService;
+
             Title = Secrets.MainLabel;
+
+            AwesomeButtonCommand = new DelegateCommand(
+                        async() => await PerformAwesomeButtonLogic(), 
+                        CanPerformAwesomeButtonLogic)
+                .ObservesProperty(() => IsAwesomeButtonEnabled)
+                ;
+
+            Device.StartTimer(TimeSpan.FromSeconds(5), () =>
+            {
+                IsAwesomeButtonEnabled = true;
+                return false;
+            });
+        }
+
+        private bool CanPerformAwesomeButtonLogic()
+        {
+            return IsAwesomeButtonEnabled;
+        }
+
+        private async Task PerformAwesomeButtonLogic()
+        {
+            await _pageDialogService.DisplayAlertAsync(Secrets.HelloWorldMessage, Secrets.HelloWorldMessage, "OK");
         }
     }
 }
